77c04395daeb011de04675ea7ff721d159f1d45b
dhrithi <dhriti280@gmail.com>
2023-10-22 23:46:44 -0700

    image resizing option

--- handler.js
+++ handler.js
@@ -1,8 +1,41 @@
-module.exports.hello = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Hello from Serverless!',
-    }),
-  };
+const jimp = require('jimp');
+const axios = require('axios');
+const AWS = require('aws-sdk');
+
+const s3 = new AWS.S3();
+
+module.exports.resize = async (event) => {
+  try {
+    const body = JSON.parse(event.body);
+    const imageUrl = body.url;
+    const width = parseInt(body.width);
+    const height = parseInt(body.height);
+
+    const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+    const buffer = Buffer.from(response.data, 'binary');
+
+    const image = await jimp.read(buffer);
+    const resizedImageBuffer = await image.resize(width, height).getBufferAsync(jimp.MIME_JPEG);
+
+    const s3Params = {
+      Bucket: 'ex-4-servless-frmewrk',
+      Key: `resized/${Date.now()}.jpg`,
+      Body: resizedImageBuffer,
+      ContentType: 'image/jpeg',
+    };
+
+    await s3.putObject(s3Params).promise();
+
+    const imageUrlS3 = `https://ex-4-servless-frmewrk.s3.amazonaws.com/${s3Params.Key}`;
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ url: imageUrlS3 }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: error.message }),
+    };
+  }
 };
